28094a1a50b16242ead9681f52de7be0ce484c1a
Sean Leas <phoenix_24leas.com>
2020-12-01 21:54:41 -0700

    Add styling

--- src/Main.js
+++ src/Main.js
@@ -31,4 +31,72 @@ export default class Main extends Component {
     };
   }
 
+  render() {
+    const { name, pic, types, desc, searchInput, isLoading } = this.state; // extract the Pokémon data from the state
+    return (
+      <SafeAreaView style={styles.wrapper}>
+        <View style={styles.container}>
+          <View style={styles.headContainer}>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                style={styles.textInput}
+                onChangeText={(searchInput) => this.setState({ searchInput })}
+                value={this.state.searchInput}
+                placeholder="Search Pokémon"
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                onPress={this.searchPokemon}
+                title="Search"
+                color="#0064e1"
+              />
+            </View>
+          </View>
+
+          <View style={styles.mainContainer}>
+            {isLoading && <ActivityIndicator size="large" color="#0064e1" />}
+
+            {!isLoading && (
+              <Pokemon name={name} pic={pic} types={types} desc={desc} />
+            )}
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
+
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  headContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  textInputContainer: {
+    flex: 2,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  mainContainer: {
+    flex: 9,
+  },
+  textInput: {
+    height: 35,
+    marginBottom: 10,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    backgroundColor: '#eaeaea',
+    padding: 5,
+  },
+});
