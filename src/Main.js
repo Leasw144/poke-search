8a3938d7baa79774ec975aa686bdd9fa99126615
Sean Leas <phoenix_24leas.com>
2020-12-01 19:59:44 -0700

    Initialize files

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  ActivityIndicator,
+} from 'react-native';
+
+export default class Main extends Component {
+  render() {
+    return null;
+  }
+}
---
+++ src/components/Pokemon.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+const Pokemon = () => null;
